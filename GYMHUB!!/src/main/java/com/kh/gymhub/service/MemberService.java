6a57aaf4cc3317e9fada296bbd9dcaade650bfb0
Hheokjina <lsh6439ll@gmail.com>
2025-11-12 10:12:46 +0900

    ..

--- GYMHUB!!/src/main/java/com/kh/gymhub/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.kh.gymhub.service;
-
-import com.kh.gymhub.model.vo.Member;
-
-public interface MemberService {
-    Member getMemberById(String memberId);
-    int getMemberCountById(String memberId);
-    int addMember(Member member);
-}
